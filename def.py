7fd4f74ec76583b5176466c87b56217f22a7f3d2
Sajan Tamang <sajanyonjan14@gmail.com>
2021-05-26 10:20:00 +0545

    fourth number

--- def.py
+++ def.py
@@ -1,6 +1,6 @@
 a = int(input("Enter a first number\t:"))
 b = int(input("Enter a second number\t:"))
-c = int(input("Enter a third number\t:"))
+c = int(input("Enter a fourth number\t:"))
 def git(a, b):
     x = a + b
     return x
